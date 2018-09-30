07834b718cfcec956c5fd1d89f03eb53a3a6fa1b
Rodrigo Vianna <rodrigogvianna@outlook.com>
2018-09-30 09:46:19 -0300

    Rounded Image custom class

--- BeMyHero/Custom View/RoundedImage.swift
+++ BeMyHero/Custom View/RoundedImage.swift
@@ -6,4 +6,26 @@
 //  Copyright © 2018 Rodrigo Vianna. All rights reserved.
 //
 
-import Foundation
+import UIKit
+
+class RoundedImage: UIImageView {
+    @IBInspectable
+    var cornerRadius: CGFloat = 0 {
+        didSet {
+            if self.cornerRadius > 0 {
+                self.layer.cornerRadius = self.cornerRadius
+            }
+            self.layer.masksToBounds = self.cornerRadius > 0
+            setNeedsDisplay()
+        }
+    }
+    
+    
+    @IBInspectable
+    var roundedCorners: Bool = false {
+        didSet {
+            self.cornerRadius = min(self.frame.size.width, self.frame.size.height) / 2
+            setNeedsDisplay()
+        }
+    }
+}
